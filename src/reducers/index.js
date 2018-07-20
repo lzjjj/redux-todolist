793ec00f7b7e56e8e3c84cc4e4d1b67b83ee763f
louis <louis.z.j.li@oocl.com>
2018-07-20 12:41:36 +0800

    [louis] refacter the code for reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,41 +6,39 @@ export default (state = {
     switch (action.type) {
         case "add":
             {
-                let todoList = state.todoList.concat()
+                let todoList = [...state.todoList]
                 todoList.push(action.item)
-                console.log(action)
-                return Object.assign({}, state, {
+                return {
                     todoList: todoList,
                     status: state.status
-                })
+                }
             }
 
         case "check":
             {
                 let todoList = state.todoList.concat()
-                console.log(action.itemId)
                 todoList.find(i => i.id == action.itemId).isComplete = action.isCheck
-                return Object.assign({}, state, {
+                return {
                     todoList: todoList,
                     status: state.status
-                })
+                }
             }
         case "status":
             {
                 let todoList = state.todoList.concat()
-                return Object.assign({}, state, {
+                return {
                     todoList: todoList,
                     status: action.status
-                })
+                }
             }
             case "edit":
             {
                 let todoList = state.todoList.concat()
                 todoList.find(i => i.id == action.itemId).content = action.content
-                return Object.assign({}, state, {
+                return {
                     todoList: todoList,
                     status: state.status
-                })
+                }
             }
 
         default:
